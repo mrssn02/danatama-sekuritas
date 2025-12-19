4365930efd881651ed583d2eb6371af7379f6a14
mrssn02 <sonandra111@gmail.com>
2025-12-19 23:13:57 +0700

    Create page.js

---
+++ danatama-simple/app/kontak/page.js
@@ -0,0 +1,10 @@
+export default function Kontak() {
+  return (
+    <>
+      <h1>Kontak Kami</h1>
+      <p>Email: info@danatama.co.id</p>
+      <p>Telepon: (021) 12345678</p>
+      <p>Alamat: Jakarta, Indonesia</p>
+    </>
+  );
+}
