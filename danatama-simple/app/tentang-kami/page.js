7bf77c914f6e8c6cb7c6ba99eda3b225ff0fea39
mrssn02 <sonandra111@gmail.com>
2025-12-19 23:12:59 +0700

    Create page.js

---
+++ danatama-simple/app/tentang-kami/page.js
@@ -0,0 +1,16 @@
+export default function TentangKami() {
+  return (
+    <>
+      <h1>Tentang Kami</h1>
+      <p>
+        PT. Danatama Makmur Sekuritas adalah perusahaan jasa keuangan
+        yang bergerak di bidang perantara perdagangan efek.
+      </p>
+
+      <p>
+        Kami berkomitmen memberikan layanan terbaik, transparan,
+        dan sesuai dengan regulasi pasar modal Indonesia.
+      </p>
+    </>
+  );
+}
