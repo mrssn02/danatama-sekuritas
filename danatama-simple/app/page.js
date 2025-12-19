f3b2730eaaf4c88a2f0ace0ea0b31a18b0022cf8
mrssn02 <sonandra111@gmail.com>
2025-12-19 22:59:07 +0700

    Update page.js

--- danatama-simple/app/page.js
+++ danatama-simple/app/page.js
@@ -1,8 +1,26 @@
 export default function Home() {
   return (
-    <main style={{ padding: 40 }}>
-      <h1>PT. Danatama Makmur Sekuritas</h1>
-      <p>Website masih dalam tahap pengembangan.</p>
-    </main>
+    <>
+      <h1>Selamat Datang</h1>
+      <p>
+        Website resmi PT. Danatama Makmur Sekuritas.
+        Saat ini website masih dalam tahap pengembangan.
+      </p>
+
+      <div style={{
+        marginTop: 32,
+        padding: 24,
+        background: "white",
+        borderRadius: 12,
+        maxWidth: 600
+      }}>
+        <h3>Informasi</h3>
+        <ul>
+          <li>Perusahaan sekuritas</li>
+          <li>Anggota Bursa Efek Indonesia</li>
+          <li>Konten akan segera diperbarui</li>
+        </ul>
+      </div>
+    </>
   );
 }
